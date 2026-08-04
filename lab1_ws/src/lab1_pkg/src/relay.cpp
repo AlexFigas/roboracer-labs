1493335c2f2419c6abb0c7472ff9236fbe2e8619
Alexandre Figueiredo <alexfigas11@gmail.com>
2026-08-04 10:17:32 +0100

    New clang-format

--- lab1_ws/src/lab1_pkg/src/relay.cpp
+++ lab1_ws/src/lab1_pkg/src/relay.cpp
@@ -3,38 +3,38 @@
 
 using std::placeholders::_1;
 
-class Relay : public rclcpp::Node {
- public:
-  Relay() : Node("relay") {
-    sub_ =
-        this->create_subscription<ackermann_msgs::msg::AckermannDriveStamped>(
+class Relay : public rclcpp::Node
+{
+  public:
+    Relay() : Node("relay")
+    {
+        sub = this->create_subscription<ackermann_msgs::msg::AckermannDriveStamped>(
             "drive", 10, std::bind(&Relay::callback, this, _1));
 
-    pub_ = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>(
-        "drive_relay", 10);
-  }
+        pub = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>("drive_relay", 10);
+    }
 
- private:
-  void callback(
-      const ackermann_msgs::msg::AckermannDriveStamped::SharedPtr msg) {
-    ackermann_msgs::msg::AckermannDriveStamped out;
+  private:
+    void callback(const ackermann_msgs::msg::AckermannDriveStamped::SharedPtr msg)
+    {
+        ackermann_msgs::msg::AckermannDriveStamped out;
 
-    out.drive.speed = msg->drive.speed * 3.0;
-    out.drive.steering_angle = msg->drive.steering_angle * 3.0;
+        out.drive.speed = msg->drive.speed * 3.0;
+        out.drive.steering_angle = msg->drive.steering_angle * 3.0;
 
-    std::cout << "Relaying: speed = " << out.drive.speed
-              << ", steering_angle = " << out.drive.steering_angle << std::endl;
-    pub_->publish(out);
-  }
+        std::cout << "Relaying: speed = " << out.drive.speed << ", steering_angle = " << out.drive.steering_angle
+                  << std::endl;
+        pub->publish(out);
+    }
 
-  rclcpp::Subscription<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr
-      sub_;
-  rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr pub_;
+    rclcpp::Subscription<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr sub;
+    rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr pub;
 };
 
-int main(int argc, char** argv) {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<Relay>());
-  rclcpp::shutdown();
-  return 0;
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Relay>());
+    rclcpp::shutdown();
+    return 0;
 }
--- lab1_ws/src/lab1_pkg/src/talker.cpp
+++ lab1_ws/src/lab1_pkg/src/talker.cpp
@@ -3,40 +3,42 @@
 
 using std::placeholders::_1;
 
-class Talker : public rclcpp::Node {
- public:
-  Talker() : Node("talker") {
-    this->declare_parameter("v", 0.0);
-    this->declare_parameter("d", 0.0);
+class Talker : public rclcpp::Node
+{
+  public:
+    Talker() : Node("talker")
+    {
+        this->declare_parameter("v", 0.0);
+        this->declare_parameter("d", 0.0);
 
-    pub_ = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>(
-        "drive", 10);
+        pub = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>("drive", 10);
 
-    timer_ = this->create_wall_timer(std::chrono::milliseconds(1),
-                                     std::bind(&Talker::publish_msg, this));
-  }
+        timer = this->create_wall_timer(std::chrono::milliseconds(1), std::bind(&Talker::publish_msg, this));
+    }
 
- private:
-  void publish_msg() {
-    double v = this->get_parameter("v").as_double();
-    double d = this->get_parameter("d").as_double();
+  private:
+    void publish_msg()
+    {
+        double v = this->get_parameter("v").as_double();
+        double d = this->get_parameter("d").as_double();
 
-    ackermann_msgs::msg::AckermannDriveStamped msg;
-    msg.drive.speed = v;
-    msg.drive.steering_angle = d;
+        ackermann_msgs::msg::AckermannDriveStamped msg;
+        msg.drive.speed = v;
+        msg.drive.steering_angle = d;
 
-    std::cout << "Publishing: speed = " << msg.drive.speed
-              << ", steering_angle = " << msg.drive.steering_angle << std::endl;
-    pub_->publish(msg);
-  }
+        std::cout << "Publishing: speed = " << msg.drive.speed << ", steering_angle = " << msg.drive.steering_angle
+                  << std::endl;
+        pub->publish(msg);
+    }
 
-  rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr pub_;
-  rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr pub;
+    rclcpp::TimerBase::SharedPtr timer;
 };
 
-int main(int argc, char** argv) {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<Talker>());
-  rclcpp::shutdown();
-  return 0;
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Talker>());
+    rclcpp::shutdown();
+    return 0;
 }
